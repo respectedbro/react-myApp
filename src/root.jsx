e69d09f4f4af20f9864f642943de45ad9ec5774e
resAsus <respectedbro@yandex.ru>
2025-06-29 16:11:42 +0300

    Initial commit

--- src/root.jsx
+++ src/root.jsx
@@ -6,9 +6,11 @@ function Root() {
             <h1>Список дел</h1>
             <ul>
                 {tasks.map((item, index) => (
-                    <li> key={index}>{item}</li>
+                    <li key={index}>{item}</li>
                 ))}
             </ul>
         </>
     );
 }
+
+export default Root
