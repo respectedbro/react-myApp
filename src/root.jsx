8751b2661e29e73465691e1f3495fc56ceac2951
resAsus <respectedbro@yandex.ru>
2025-06-29 15:57:49 +0300

    Initial commit

---
+++ src/root.jsx
@@ -0,0 +1,14 @@
+const tasks = ['Помыть посуду', 'Сделать домашнее задание', 'Почитать книгу'];
+
+function Root() {
+    return (
+        <>
+            <h1>Список дел</h1>
+            <ul>
+                {tasks.map((item, index) => (
+                    <li> key={index}>{item}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
